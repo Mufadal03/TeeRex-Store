63e104be276e4575f65635550ce752fe16c910a1
Mufadal03 <sadrimufadal@gmail.com>
2023-01-15 15:28:16 +0530

    search bar setup

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "../App.css"
+const SearchBar = () => {
+  return (
+              <div className="searchbar">search bar</div>
+
+  )
+}
+
+export default SearchBar
