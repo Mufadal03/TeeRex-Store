4b1acab72117c16740851ed757b21f79b5be294f
Mufadal03 <sadrimufadal@gmail.com>
2023-01-15 17:11:14 +0530

    category comp

---
+++ src/components/Category.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Options from './Options'
+
+const Category = ({heading,data}) => {
+  return (
+      <div className='category-box'>
+          <h3>{heading}</h3>
+          {data.map((el) => <Options name={el}/>)}
+    </div>
+  )
+}
+
+export default Category
