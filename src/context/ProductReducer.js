cbe509883726b645dced78f49a4bff36b78e873e
Mufadal03 <sadrimufadal@gmail.com>
2023-01-15 18:50:15 +0530

    logic for ADD REMOVE QTY CHANGE

--- src/context/ProductReducer.js
+++ src/context/ProductReducer.js
@@ -1,6 +1,6 @@
 const initialState = {
     products: [],
-    cart:[]
+    cart:[ ]
 }
 
 export const ProductReducer = (state=initialState, { type, payload }) => {
@@ -9,6 +9,24 @@ export const ProductReducer = (state=initialState, { type, payload }) => {
             return {
                 ...state,
                 products:[...payload]
+            }
+        }
+        case "ADD_TO_CART": {
+            return {
+                ...state,
+                cart:[...state.cart,{...payload,cartQuantity:1}]
+            }
+        }
+        case "REMOVE_FROM_CART": {
+            return {
+                ...state,
+                cart:state.cart.filter((p)=>p.id!=payload)
+            }
+        }
+        case "CHANGE_QTY": {
+            return {
+                ...state,
+                cart:state.cart.filter((p)=>p.id==payload[0].id?p.cartQuantity=payload[1]:p)
             }
         }
         default:return state
