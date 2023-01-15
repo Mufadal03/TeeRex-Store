b25816cc6156a91b11f2b158de990eecf9c5c514
Mufadal03 <sadrimufadal@gmail.com>
2023-01-15 21:16:50 +0530

    done with all functionalities filter search done

--- src/routes/Products.jsx
+++ src/routes/Products.jsx
@@ -6,20 +6,73 @@ import SearchBar from '../components/SearchBar'
 import Filters from '../components/Filters'
 import Card from '../components/Card'
 const Products = () => {
-    const { state: { products ,cart },dispatch } = useContext(ProductContext)
-    console.log(products,cart)
+    let { state: { products}, dispatch ,filterState,filterState:{searchQuery,Color,Gender,Price,Type}} = useContext(ProductContext)
     useEffect(() => {
             Fetch()
             .then((r) =>dispatch({ type: "GET_PRODUCTS", payload: r }))
             .catch((e) => console.log(e))
-    },[])
+    }, [])
+    const transFormation = () => {
+        let result = products
+        // text search 
+        if (searchQuery) {
+            searchQuery=searchQuery.toLowerCase()
+            result = products.filter((el) => {
+                if(el.name.toLowerCase()===searchQuery || el.color.toLowerCase()===searchQuery || el.gender.toLowerCase()===searchQuery || el.type.toLowerCase()===searchQuery || el.price==searchQuery)return true
+            })
+        }
+        // filter color
+        if (Color.length > 0) {
+            let ans = []
+            for (let i = 0; i < Color.length; i++){
+               ans=[...ans,...result.filter((el)=>el.color===Color[i])]
+            }
+            result =ans
+        }
+
+        // filter Gender
+        if (Gender.length > 0) {
+            let ans = []
+            for (let i = 0; i < Gender.length; i++){
+               ans=[...ans,...result.filter((el)=>el.gender===Gender[i])]
+            }
+            result =ans
+        }
+        // filter type
+        if (Type.length > 0) {
+            let ans = []
+            for (let i = 0; i < Type.length; i++){
+               ans=[...ans,...result.filter((el)=>el.type===Type[i])]
+            }
+            result =ans
+        }
+        // filter by price 
+        if (Price.length > 0) {
+            let ans = []
+            for (let i = 0; i < Price.length; i++){
+                let [min, max] = Price[i].split("-")
+                // console.log(min,max)
+                ans = [...ans, ...result.filter((el) => {
+                   if(+el.price>=+min && +el.price<=+max)return true
+               })]
+            }
+            result =ans
+        }
+
+
+
+        console.log("result",result)
+        return result
+    }
+    console.log(filterState)
   return (
       <main className='product-container-main'>
           <SearchBar />
           <div className="product-container">
               <Filters />
               <div className="products">
-                  {products?.length > 0 && products?.map((el, i) => <Card key={i}{...el} />)}
+          {transFormation().length===0 && <p>Searched Product not found</p>}
+                  {transFormation().map((el, i) => <Card key={i}{...el} />)}
               </div>
           </div>
     </main>
