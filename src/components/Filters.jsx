2e42c7c937ff17f6f9809e346ecdffc33d6962d5
Mufadal03 <sadrimufadal@gmail.com>
2023-01-15 21:13:08 +0530

    filter logic done

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -5,9 +5,9 @@ const Filters = () => {
   return (
       <div className='filter-main'>
           <div className="filter-container">
-              <Category heading={"Color"} data={["Red", "Blue", "Green"]} />
+              <Category heading={"Color"} data={["Red", "Blue", "Green","Yellow","Grey"]} />
               <Category heading={"Gender"} data={["Men", "Women"]} />
-              <Category heading={"Price"} data={["0-250","251-450","450"] } />
+              <Category heading={"Price"} data={["0-250","251-300","350-450"] } />
               <Category heading={"Type"} data={["Polo","Hoodie","Basic"] } />
         </div>
    </div>
