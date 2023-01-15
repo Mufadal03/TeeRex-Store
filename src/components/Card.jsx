eb9a37dd852b79a6109699ef51485ad35dda8632
Mufadal03 <sadrimufadal@gmail.com>
2023-01-15 18:48:30 +0530

    add and remove functionality

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import { useContext } from 'react'
 import "../App.css"
+import { ProductContext } from '../context/Product.context'
 const Card = (props) => {
-    const {imageURL,color,gender,id,name,price,quantity,type} = props
+    const { imageURL, color, gender, id, name, price, quantity, type } = props
+    const { state: { cart }, dispatch } = useContext(ProductContext)
   return (
       <div className='card'>
           <img className='prod-img' src={imageURL} alt={name} />
@@ -12,7 +15,12 @@ const Card = (props) => {
               <p>Price: {price}Rs</p>
           </div>
           <div className="add-to-cart">
-              <button>Add to Cart</button>
+              {
+                  cart.some((p) => p.id == props.id) ?
+                      <button className='remove-from-cart-btn' onClick={()=>dispatch({type:"REMOVE_FROM_CART",payload:props.id})}>Remove from Cart</button> :
+                      <button className='add-to-cart-btn' onClick={() => dispatch({ type: "ADD_TO_CART", payload: props })}>Add to Cart</button>
+              }
+              
           </div>
     </div>
   )
