05957948e4cd4523fe56dc6f0c9b49077f9aad3d
Mufadal03 <sadrimufadal@gmail.com>
2023-01-15 21:14:08 +0530

    done with search functionalities

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { useContext } from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
 import "../App.css"
+import { ProductContext } from '../context/Product.context'
 const SearchBar = () => {
+  const [text, setText] = useState("")
+  const {dispatchFilter} = useContext(ProductContext)
+  useEffect(() => {
+    let timer = setTimeout(() => {
+        dispatchFilter({type:"GET_SEARCH",payload:text})
+    }, [500])
+    return ()=>clearTimeout(timer)
+  },[text])
   return (
       <div className="searchbar">
-          <input type="search" placeholder='Search products' />
+          <input type="search" placeholder='Search products'value={text} onChange={(e)=>setText(e.target.value)} />
      </div>
   )
 }
