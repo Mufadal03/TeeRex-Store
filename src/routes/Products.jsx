865c990fb83a964609bb9c88ee0f6b470852d40d
Mufadal03 <sadrimufadal@gmail.com>
2023-01-15 15:31:22 +0530

    products page

---
+++ src/routes/Products.jsx
@@ -0,0 +1,29 @@
+import React, { useContext, useEffect } from 'react'
+import { ProductContext } from '../context/Product.context'
+import { Fetch } from '../logic/Fetch'
+import "../App.css"
+import SearchBar from '../components/SearchBar'
+import Filters from '../components/Filters'
+import Card from '../components/Card'
+const Products = () => {
+    const { state: { products ,cart },dispatch } = useContext(ProductContext)
+    console.log(products,cart)
+    useEffect(() => {
+            Fetch()
+            .then((r) =>dispatch({ type: "GET_PRODUCTS", payload: r }))
+            .catch((e) => console.log(e))
+    },[])
+  return (
+      <main className='product-container-main'>
+          <SearchBar />
+          <div className="product-container">
+              <Filters />
+              <div className="products">
+                  {products?.length > 0 && products?.map((el, i) => <Card {...el} />)}
+              </div>
+          </div>
+    </main>
+  )
+}
+
+export default Products
