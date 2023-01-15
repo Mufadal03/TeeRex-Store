1bfdb81677a2d0b8cd3d20e69e3ba2870236821d
Mufadal03 <sadrimufadal@gmail.com>
2023-01-15 17:12:56 +0530

    filter component : done

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import Category from './Category'
 
 const Filters = () => {
   return (
       <div className='filter-main'>
           <div className="filter-container">
-                  filters
+              <Category heading={"Color"} data={["Red", "Blue", "Green"]} />
+              <Category heading={"Gender"} data={["Men", "Women"]} />
+              <Category heading={"Price"} data={["0-250","251-450","450"] } />
+              <Category heading={"Type"} data={["Polo","Hoodie","Basic"] } />
         </div>
    </div>
   )
