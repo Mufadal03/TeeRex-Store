6eb533e542400a92887218def8b04b937c20ae7a
Mufadal03 <sadrimufadal@gmail.com>
2023-01-15 21:13:36 +0530

    Dynamic options added

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import { useContext } from 'react'
+import { ProductContext } from '../context/Product.context'
 
-const Options = ({name}) => {
+const Options = ({ name, section }) => {
+  const { dispatchFilter } = useContext(ProductContext)
+  const handleChange = (e) => {
+    dispatchFilter({type:"CHANGE_CATEGORY",payload:[section,name]})
+  }
   return (
       <div className='category-option'>
-          <input type={"checkbox"} id={ name} />
+          <input type={"checkbox"} name={name} data_belongs={section} onChange={handleChange} id={ name} />
           <label htmlFor={name} >{name }</label>
     </div>
   )
 }
-
+ 
 export default Options
